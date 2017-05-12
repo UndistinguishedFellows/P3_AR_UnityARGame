434a98031e2f94260fc31ef18ca53bdfc46f9378
Josef21296 <jcasanovasv@hotmail.com>
2017-05-12 23:15:02 +0200

    Changes on game movement mechanics
    TODO: Must add a representation of targets

--- RA_P3_UnityAR/Assets/_ASSETS/Scripts/DestroyByBoundary.cs
+++ RA_P3_UnityAR/Assets/_ASSETS/Scripts/DestroyByBoundary.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DestroyByBoundary : MonoBehaviour {
 
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         Destroy(other.gameObject);
     }
