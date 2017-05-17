4950c17f39633e15712618cd5baba41cdb0c9e07
Josef21296 <jcasanovasv@hotmail.com>
2017-05-17 11:35:25 +0200

    Column targets

--- RA_P3_UnityAR/Assets/_ASSETS/Scripts/Scripts revision/WaveSpawner.cs
+++ RA_P3_UnityAR/Assets/_ASSETS/Scripts/Scripts revision/WaveSpawner.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public GameObject enemyPrefab;
+
+	public Transform columnSpawn1;
+	public Transform columnSpawn2;
+
+	Vector3 spawnLine;
+
+	void Start () 
+	{
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update ()
+	{
+		spawnLine = columnSpawn2.position - columnSpawn1.position;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine (columnSpawn1.position, columnSpawn2.position);
+
+		float dist = spawnLine.magnitude / 2;
+		Vector3 midPos = columnSpawn1.position + new Vector3(dist, dist, dist);
+
+		Gizmos.DrawSphere (midPos, 0.2f);
+		//Gizmos.DrawLine ();
 	}
 }
