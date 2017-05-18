4f9b02e9eab3657bb76bd4d216c91f5ba3ff39ea
CapitanLiteral <anaraguen@gmail.com>
2017-05-18 02:29:41 +0200

    Button

---
+++ RA_P3_UnityAR/Assets/_ASSETS/Scripts/AlphaButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AlphaButton : MonoBehaviour
+{
+    private Image img;
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    void OnClick()
+    {
+        Color color = img.color;
+        color.a = 200;
+        img.color = color;
+    }
+}
